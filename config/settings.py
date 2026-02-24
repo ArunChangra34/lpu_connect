8c625e293429064d809cba2694d6609b00e72997
Arun Changra <arxn@Aruns-MacBook-Air-2.local>
2026-02-24 11:38:33 +0530

    Fix superuser creation properly

--- config/settings.py
+++ config/settings.py
@@ -138,16 +138,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'signup'
 
-import os
 
-if os.environ.get("DJANGO_SUPERUSER_USERNAME"):
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-
-    if not User.objects.filter(username=os.environ.get("DJANGO_SUPERUSER_USERNAME")).exists():
-        User.objects.create_superuser(
-            username=os.environ.get("DJANGO_SUPERUSER_USERNAME"),
-            email=os.environ.get("DJANGO_SUPERUSER_EMAIL"),
-            password=os.environ.get("DJANGO_SUPERUSER_PASSWORD"),
-        )
 
