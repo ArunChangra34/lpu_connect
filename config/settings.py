b441724c446bf9ea30cbced68c1001e4035a9e7e
Arun Changra <arxn@Aruns-MacBook-Air-2.local>
2026-05-07 22:32:16 +0530

    fixed database config

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
 }
